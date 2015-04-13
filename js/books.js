5366a8a7e885fff7f5864955d207f35b14ad2ba0
nmadov <madame@gmail.com>
2015-04-13 19:46:06 -0400

    2nd commit

---
+++ js/books.js
@@ -0,0 +1,14 @@
+"books": [
+	{
+		"title":"Night of the Gun",
+		"author":"David Carr"
+	},
+	{
+		"title":"Wuthering Heights",
+		"author":"Emily Brontë"
+	},
+	{
+		"title":"Atonement",
+		"author":"Ian McEwan"
+	}
+]
